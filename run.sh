0aca5343605d85d67eb4fa49b26e253370fb893c
gpi <gpimpale29@gmail.com>
2020-11-15 00:46:37 -0800

    simplify

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# per deployment settings
-# check out application.properties
-# to see what env vars you need to export
-source settings.sh
-
-java -jar  ./build/libs/hours-0.0.1-SNAPSHOT.jar
